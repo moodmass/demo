efe01866ddfd7db3c3d79745f7014fe639f76b64
leo.macray <leo.macray@gmail.com>
2016-09-01 17:43:56 +0800

    build

--- app/src/main/java/com/hy/mdms/demo/QuizActivity.java
+++ app/src/main/java/com/hy/mdms/demo/QuizActivity.java
@@ -2,12 +2,16 @@ package com.hy.mdms.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        Log.d(TAG, "My first App");
     }
 }
